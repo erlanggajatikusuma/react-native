36fa7de470a61973982c3555cf1c8f9db544e6dd
erlanggajatikusuma <erlanggajatikusuma007@gmail.com>
2020-12-09 07:59:21 +0700

    setup eslint

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import {View, ScrollView} from 'react-native';
-import SampleComponent from './pages/SampleComponent';
-import StylingComponent from './pages/StylingComponent'
-import FlexBox from './pages/FlexBox';
-import Positon from './pages/Position';
-import PropsDinamis from './pages/PropsDimamis';
-import StateDinamis from './pages/StateDinamis';
+// import SampleComponent from './pages/SampleComponent';
+// import StylingComponent from './pages/StylingComponent'
+// import FlexBox from './pages/FlexBox';
+// import Positon from './pages/Position';
+// import PropsDinamis from './pages/PropsDimamis';
+// import StateDinamis from './pages/StateDinamis';
 import Communication from './pages/Communication';
 
 const App = () => {
@@ -21,7 +21,7 @@ const App = () => {
         <Communication />
       </ScrollView>
     </View>
-  )
-}
+  );
+};
 
 export default App;
